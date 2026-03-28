74585b9f1d0a350585b7104cbaebb15157ff38ba
nikita <nikita_timoshenko@outlook.com>
2026-03-29 00:45:04 +0300

    Install custom nodes for ComfyUI
    Added functionality to install custom nodes from GitHub repositories before starting ComfyUI.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,23 @@ set -e
 echo "Running model copy..."
 /bin/bash /copy_models.sh
 
-# FIX runpod bug
 mkdir -p /workspace/runpod-slim
 touch /workspace/runpod-slim/comfyui_args.txt
 
-echo "Starting original RunPod start.sh..."
+echo "Installing custom nodes..."
+cd /workspace/ComfyUI/custom_nodes
+
+install_if_missing () {
+    if [ ! -d "$2" ]; then
+        git clone "$1" "$2"
+    fi
+}
+
+install_if_missing https://github.com/Kijai/ComfyUI-WanVideoWrapper ComfyUI-WanVideoWrapper
+install_if_missing https://github.com/Kijai/ComfyUI-KJNodes ComfyUI-KJNodes
+install_if_missing https://github.com/yolain/ComfyUI-Easy-Use ComfyUI-Easy-Use
+install_if_missing https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite ComfyUI-VideoHelperSuite
+install_if_missing https://github.com/Kijai/ComfyUI-WanAnimatePreprocess ComfyUI-WanAnimatePreprocess
+
+echo "Starting ComfyUI..."
 exec /start.sh
