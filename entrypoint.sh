dbc917dbab9a2b37a9b5f0c1e5b29cf3611c7713
nikita <nikita_timoshenko@outlook.com>
2026-03-28 23:55:06 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@ set -e
 echo "Running model copy..."
 /bin/bash /copy_models.sh
 
+# FIX runpod bug
+mkdir -p /workspace/runpod-slim
+touch /workspace/runpod-slim/comfyui_args.txt
+
 echo "Starting original RunPod start.sh..."
 exec /start.sh
